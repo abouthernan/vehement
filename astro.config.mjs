d8cf6d1744421c42e0710a6e3f5390ae4e11baeb
Cristhian Melo <rxtsel@hotmail.com>
2023-04-11 00:45:04 -0500

    chore(dep): remove image component and sharp

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  })]
+  integrations: [react()]
 });
