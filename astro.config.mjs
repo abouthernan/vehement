fdb0805669db8121bf81bcf69e4d331a52d7fd2d
Cristhian Melo <rxtsel@hotmail.com>
2023-04-12 16:50:13 -0500

    feat: add sitemaps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import astroI18next from "astro-i18next";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), astroI18next()],
+  integrations: [react(), astroI18next(), sitemap()],
   site: "https://vehement.co",
   base: '/'
 });
