d667458bc74f9229a751e2f4562ec122857abc58
Cristhian Melo <rxtsel@hotmail.com>
2023-04-12 13:50:47 -0500

    feat: add config to github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,6 @@ import astroI18next from "astro-i18next";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), astroI18next()],
+  site: "https://abouthernan.github.io",
+  base: '/vehement'
 });
