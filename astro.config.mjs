bcd015295075ac48e76bc2e443dbfaf86c30228a
Cristhian Melo <rxtsel@hotmail.com>
2024-01-17 18:04:01 -0500

    chore: change astro config\

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
-import react from "@astrojs/react";
-import astroI18next from "astro-i18next";
+import react from '@astrojs/react';
+import astroI18next from 'astro-i18next';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), astroI18next()],
-  site: "https://abouthernan.github.io",
-  base: '/vehement'
+  site: 'https://vehement.co',
+  base: '/'
 });
